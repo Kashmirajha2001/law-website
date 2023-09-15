4a67ff5d8061a82cba7761fa48bf0930195c04b4
Adnan Sameer <adnansameer400@gmail.com>
2023-09-16 01:29:43 +0530

    Added News Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Intro from './components/Intro/Intro';
 import Carousel from './components/Carousel/Carousel';
 import Footer from './components/Footer/Footer';
 import PracticeAreas from './components/PracticeAreas/PracticeAreas';
+import News from './components/News/News';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Intro/>
       <Nav/>
       <Carousel/>
+      <News/>
       <PracticeAreas/>
       <Footer/>
     </>
