68927e9ca3275a596a7d4829085794024200cb90
Adnan Sameer <adnansameer400@gmail.com>
2023-09-16 13:47:19 +0530

    Updated Chat.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,14 @@ import Carousel from './components/Carousel/Carousel';
 import Footer from './components/Footer/Footer';
 import PracticeAreas from './components/PracticeAreas/PracticeAreas';
 import News from './components/News/News';
+import Chat from './components/Chat/Chat';
 
 function App() {
   return (
     <>
       <Intro/>
       <Nav/>
+      <Chat/>
       <Carousel/>
       <News/>
       <PracticeAreas/>
