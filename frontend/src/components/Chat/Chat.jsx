abf055c72b0e44ddbd804f441eb173cb84608645
Pranjal <tataisarkar2001@gmail.com>
2023-09-20 14:35:36 +0530

    chat bot: was able to post requests from front end to backend api using axios

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './chat.css';
-import { SiProbot } from 'react-icons/si';
+import { SiAxios, SiProbot } from 'react-icons/si';
+import axios from 'axios';
 
 const Chat = () => {
     const [showDiv, setShowDiv] = useState(false);
@@ -8,6 +9,8 @@ const Chat = () => {
     const [userMessages, setUserMessages] = useState([]);
     const [botMessages, setBotMessages] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [chatBotText, setChatBotText] = useState();
+    const [flag, setFlag] = useState(null);
 
     
     const toggleDiv = () => {
@@ -18,17 +21,36 @@ const Chat = () => {
         setUserInput(e.target.value);
     }
 
-    const handleSendMessage = (e) => {
+    const handleSendMessage = async (e) => {
         e.preventDefault();
         setUserMessages([...userMessages, userInput]);
         setUserInput('');
-        simulateBotResponse(userInput);
+        await simulateBotResponse(userInput);
     }
 
-    const simulateBotResponse = (userInput) => {
+    const simulateBotResponse = async (userInput) => {
         setIsLoading(true);
+
+        console.log("in simulateBotResponse function.");
+
+        const botResponse = await axios.post("http://localhost:5000/", {
+            userChatText: userInput
+        })
+
+        console.log(botResponse);
+        console.log(botResponse.data[0]);
+        console.log(botResponse.data[1]);
+
+        const chatBotText2 = botResponse.data[0];
+        setChatBotText(botResponse.data[0]);
+        // console.log("chatBotText before the clg: " + chatBotText); // undefined
+        const flag2 = botResponse.data[1];
+        setFlag(botResponse.data[1]);
+
+        console.log("chatBotText: " + chatBotText2);
+
         setTimeout(() => {
-            const botResponse = `Bot: You said "${userInput}"`;
+            const botResponse = `Bot: "${chatBotText2}"`;
             setBotMessages([...botMessages, botResponse]);
             setIsLoading(false);
         }, 1000);
@@ -49,9 +71,15 @@ const Chat = () => {
                     <div className="loading-indicator">Bot is typing...</div>
                 ) : (
                     botMessages.map((message, index) => (
-                        <div key={index} className={`bot-msg`}>
-                            <div className='message-content left'>{message}</div>
-                        </div>
+                        flag === 0
+                        ?
+                        <div key={index} className={`bot-msg`}>{message}</div>
+                        :
+                        <>
+                            <a href={chatBotText} target='_blank'>
+                            <div key={index} className={`bot-msg`}>{message}</div>
+                            </a>
+                        </>
                     ))
                 )}
                 </div>
