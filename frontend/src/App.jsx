ca4d2a61b4af55b33e3c1add111ae5fcc61aeffd
Kashmira Jha <kashmirajha2001@gmail.com>
2023-09-29 11:37:32 +0530

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
-import Community from "./components/CommunitySection/Community";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/SignUp" element={<SignUp />} />
-          <Route path="/Community" element={<Community/>} />
           <Route path="*" element={<div>Error</div>} />
         </Routes>
       </BrowserRouter>
