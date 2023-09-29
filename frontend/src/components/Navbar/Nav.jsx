b5310767f27c4c97874d4e163afbf7901b8387f8
Kashmira Jha <kashmirajha2001@gmail.com>
2023-09-29 11:43:38 +0530

    ....

--- frontend/src/components/Navbar/Nav.jsx
+++ frontend/src/components/Navbar/Nav.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Hamburger from "../../assets/hamburger.svg";
 import "./nav.css";
-import { useNavigate } from 'react-router-dom';
-import adminImg from '../../assets/images/adminProfile.png';
+import { useNavigate } from "react-router-dom";
+import adminImg from "../../assets/images/adminProfile.png";
 
 const Nav = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -12,69 +12,71 @@ const Nav = () => {
     setShowNavbar(!showNavbar);
   };
 
-  const navigate = useNavigate()
-  const navigateToLogin = () =>{
-      navigate("/Login")
+  const navigate = useNavigate();
+  const navigateToLogin = () => {
+    navigate("/Login");
   };
 
-  const navigateToSignUp = () =>{
-    navigate("/SignUp")
+  const navigateToSignUp = () => {
+    navigate("/SignUp");
   };
 
   return (
     <div>
-    <nav className="navbar">
-      <div className="container">
-        <div className="menu-icon" onClick={handleShowNavbar} >
-          <img src={Hamburger} alt="" />
-        </div>
-        <div className={`nav-elements  ${showNavbar && "active"}`}>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="">About Us</a>
-            </li>
-            <li>
-              <a href="">Services</a>
-            </li>
-            <li>
-              <a href="">News</a>
-            </li>
-            {storedCredentials ? (
+      <nav className="navbar">
+        <div className="container">
+          <div className="menu-icon" onClick={handleShowNavbar}>
+            <img src={Hamburger} alt="" />
+          </div>
+          <div className={`nav-elements  ${showNavbar && "active"}`}>
+            <ul>
               <li>
-                <a onClick={() => setIsOpen(!isOpen)}>Community</a>
+                <a href="/">Home</a>
               </li>
-            ) : ''}
-            <li>
-              <a href="">Contact Us</a>
-            </li>
-            {storedCredentials ? (
-              <img className="profile-img" src={adminImg} alt="admin.png"/>
-            ) : ''}
-            {storedCredentials ? (
-              <h4 style={{color:'#fff'}}>Admin</h4>
-            ) : ''}
-            
-              {storedCredentials ? 
-                null :
-                (
+              <li>
+                <a href="">About Us</a>
+              </li>
+              <li>
+                <a href="">Services</a>
+              </li>
+              <li>
+                <a href="">News</a>
+              </li>
+              {storedCredentials ? (
+                <li>
+                  <a onClick={navigateToCommunity}>Community</a>
+                </li>
+              ) : (
+                ""
+              )}
+              <li>
+                <a href="">Contact Us</a>
+              </li>
+              {storedCredentials ? (
+                <img className="profile-img" src={adminImg} alt="admin.png" />
+              ) : (
+                ""
+              )}
+              {storedCredentials ? (
+                <h4 style={{ color: "#fff" }}>Admin</h4>
+              ) : (
+                ""
+              )}
+
+              {storedCredentials ? null : (
                 <li>
                   <a onClick={navigateToLogin}>login</a>
                 </li>
               )}
-              {storedCredentials ? 
-                null :
-                (
-                  <li>
-                    <a onClick={navigateToSignUp}>SignUp</a>
-                  </li>
+              {storedCredentials ? null : (
+                <li>
+                  <a onClick={navigateToSignUp}>SignUp</a>
+                </li>
               )}
-          </ul>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </div>
   );
 };
